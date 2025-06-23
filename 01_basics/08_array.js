7c481c911a5f6838cab2544efa374bfdfac8aa47
Sahil123-FNO <sahilkoshti4@gmail.com>
2025-06-23 09:22:41 +0000

    01_basics config done

---
+++ 01_basics/08_array.js
@@ -0,0 +1,43 @@
+//array
+
+/*
+const myArr = [0,1,2,3,4,5,true,"sahil"] 
+const myHeros = ["ironman" , "hulk"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]); //array value can be acccessed by Indexing. It's start from 0 index.
+*/
+//In array multiple type of element can be written.
+//array is an object. In that collect multiple elements by declaring single variable.
+//JS array is resizable , mix of diff.datatype, 
+// IN JS , we do array copy operations, it creates shallow copies.
+// Shallow copies means properties share the same references . when we change the value of array it changed in original array.
+//Deep copy means properties do not share the same references.
+
+
+
+
+//Array methods
+
+/*
+myArr.push(6)  //add element in array 
+myArr.push(7)
+myArr.pop()   //remove last element of array
+myArr.unshift(9) //add element in the starting of array
+myArr.shift() // remove the starting element of array.
+
+console.log(myArr.includes(9)); //if yes than true , otherwise false.
+console.log(myArr.indexOf(3));
+console.log(myArr.indexOf(14)); //if you put index value that doesn't exit than it give answer -1.
+console.log(myArr);
+
+
+const newArr = myArr.join() //add all the alement of array into string.
+console.log(newArr);
+console.log(typeof newArr);
+*/
+
+
+//slice & splice
+
+
